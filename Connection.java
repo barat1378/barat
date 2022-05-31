b985f349a1ada23fa2c7b35bbca29f0a8a6e6f7a
barat1378 <baratalihassanzada1378@gmail.com>
2022-05-31 15:27:02 +0430

    add sytem.out.println()

--- Connection.java
+++ Connection.java
@@ -3,6 +3,6 @@ package barat;
 public class Connection {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        
+        System.out.println("new Hello World");
     }
 }
